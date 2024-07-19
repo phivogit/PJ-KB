53a04ff852a1a6d2a20ab34792f853326a5c3ca1
phivogit <153927895+phivogit@users.noreply.github.com>
2024-07-19 22:09:44 +0700

    trying to create a virtual keyboard GUI P1.1

--- KB.py
+++ KB.py
@@ -17,18 +17,23 @@ def printletter(letter):
 letterlist1 = ['Q', 'U', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
 letterlist2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
 letterlist3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
-currentColumn = 0
-for letter in letterlist1:
-    ttk.Button(keyboardFrame, text=letter, command=lambda: printletter(letter)).grid(row=1, column=currentColumn)
-    currentColumn +=1
-currentColumn = 0
-for letter in letterlist2:
-    ttk.Button(keyboardFrame, text=letter, command=lambda: printletter(letter)).grid(row=2, column=currentColumn)
-    currentColumn +=1
-currentColumn = 0
-for letter in letterlist3:
-    ttk.Button(keyboardFrame, text=letter, command=lambda: printletter(letter)).grid(row=3, column=currentColumn)
-    currentColumn +=1
+letterList = {
+    'first': ['Q', 'U', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    'second': ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    'third': ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+}
+
+for krow, kletters in letterList.items():
+    if str(krow) == 'first':
+        currentRow = 1
+    elif str(krow) == 'second':
+        currentRow = 2
+    elif str(krow) == 'third':
+        currentRow = 3
+    currentColumn = 0
+    for letter in kletters:
+        ttk.Button(keyboardFrame, text=letter, command=lambda: printletter(letter)).grid(row=currentRow, column=currentColumn)
+        currentColumn+=1
 ttk.Button(keyboardFrame, text=" ", command=lambda: printletter(' ')).grid(row=4, column=5)
 
 
